42b4d2d7692ac58ccb788cacf0b2c11f0e785351
Shubham <spal5776@gmail.com>
2023-02-07 18:55:50 +0530

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
